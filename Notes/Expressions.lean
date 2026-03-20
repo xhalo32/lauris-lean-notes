e5600288948a4061d9645510ed3c340a60c3092a
Lauri Oksanen <lauri.oksanen@helsinki.fi>
2026-03-20 10:28:54 +0200

    Small polish

--- Notes/Expressions.lean
+++ Notes/Expressions.lean
@@ -6,7 +6,7 @@ tag := "sec-expressions"
 -/
 import Mathlib
 /-
-Lean is based on a typed [$`\lambda`-calculus][lambda], specifically a version of the [calculus of constructions][coc] with [inductive types][inductive-types]. We will refer to this formal system simply as _type theory_. In particular, every expression has a type. The `example` {index}[example] command checks that a given expression has the specified type.
+Lean is based on a typed [$`\lambda`-calculus][lambda], specifically a version of the [calculus of constructions][coc] with [inductive types][inductive-types]. We will refer to this formal system simply as _type theory_. In particular, every expression has a type. The `example` {index}[example] command checks that a given expression has the specified type.{margin}[Hovering over `ℕ` in [VS Code][vscode] shows that it can be entered using `\N`.]
 
 [lambda]: https://en.wikipedia.org/wiki/Lambda_calculus
 [coc]: https://en.wikipedia.org/wiki/Calculus_of_constructions
@@ -16,28 +16,33 @@ Lean is based on a typed [$`\lambda`-calculus][lambda], specifically a version o
 -/
 example : ℕ := 0
 /-
-The example asserts that `ℕ` is the type of `0`. The type `ℕ` encodes the natural numbers.{margin}[Hovering over `ℕ` in [VS Code][vscode] shows that it can be entered using `\N`.]
-
-Lean provides a substantial amount of [syntactic sugar][sugar]. For example, `ℕ` is syntactic sugar for {lean}`Nat`. Understanding the underlying type theory often requires considering expressions from which syntactic sugar has been removed.
+The example asserts that `ℕ` is the type of `0`. The type `ℕ` encodes the natural numbers. The command `#check` {index}[#check] is used to inspect the type of an expression.{margin}[Hovering over `#check` shows its output.]
 
 [vscode]: https://lean-lang.org/install/
-[sugar]: https://en.wikipedia.org/wiki/Syntactic_sugar
 
-The command `#check` {index}[#check] is used to inspect the type of an expression.{margin}[Hovering over `#check` shows its output.]
 -/
 #check 0
 /-
 The output `0 : ℕ` means that `0` has type `ℕ`. Another way to say this is that `0` inhabits `ℕ`.
 
+Lean provides a substantial amount of [syntactic sugar][sugar] and more general user-facing surface syntax. When studying the underlying type theory, it can be useful to consider expressions from which the surface syntax has been removed. [Pretty-printing][pretty-printing] can be adjusted using `set_option` command. {index}[`set_option`] For example, `ℕ` is syntactic sugar for {lean}`Nat`.
+
+[sugar]: https://en.wikipedia.org/wiki/Syntactic_sugar
+[pretty-printing]: https://en.wikipedia.org/wiki/Pretty-printing
+
 -/
-#check (0, 1)
+set_option pp.notation false in
+#check 0
 /-
-The pair `(0, 1)` has type `ℕ × ℕ`, encoding the [Cartesian product][prod] of `ℕ` with itself. This is syntactic sugar for `Prod ℕ ℕ`.
+
+The pair `(0, 1)` has type `ℕ × ℕ`, encoding the [Cartesian product][prod] of `ℕ` with itself. This is syntactic sugar for `Prod Nat Nat`.
 
 [prod]: https://en.wikipedia.org/wiki/Cartesian_product
 
 -/
-example : ℕ × ℕ := (0, 1)
+#check (0, 1)
+
+example : Prod Nat Nat := (0, 1)
 /-
 
 The following invalid example produces a type mismatch error, since the pair `(0, 0)` does not have type `ℕ`.
@@ -85,17 +90,15 @@ Propositions are expressions of type {lean}`Prop`.
 example : Prop := 0 = 0
 example : Prop := 1 = 0
 /-
-The first proposition is provable, while the second is not. In fact, the negation of the second is provable.
-
-Interestingly, the proposition `0 = 0` is itself a type. In general, all expressions of type {lean}`Prop` are themselves types.
-
-An expression of type `0 = 0` is viewed as a proof of `0 = 0`. In general, to prove a proposition in Lean is to construct an expression having the proposition as its type.
+The first proposition is provable, while the second is not.{margin}[In fact, the negation of `1 = 0` is a consequence  the Peano axioms that we prove {ref "sec-peano"}[later]] Interestingly, the proposition `0 = 0` is itself a type,{margin}[The expression `0 = 0` is syntactic sugar for `Eq 0 0`. We will {ref "sec-equality"}[return] to this later.] and an expression of type `0 = 0` encodes a proof of `0 = 0`. In general, all expressions of type {lean}`Prop` are themselves types. To prove a proposition in Lean is to construct an expression inabiting the proposition.
 -/
 example : 0 = 0 := rfl
 /-
-We will consider the precise meaning of {lean}`rfl` (and the equality `=`) {ref "sec-equality"}[later]. For the moment, let us simply view {lean}`rfl` as a canonical proof of `a = a` for any expression `a`.
+We will consider the precise meaning of {lean}`rfl` {ref "sec-equality"}[later]. For the moment, let us simply view {lean}`rfl` as a canonical proof of [reflexivity][reflexivity] of equality.
 
-Like `ℕ`, {lean}`Prop` has type {lean}`Type`.
+[reflexivity]: https://en.wikipedia.org/wiki/Reflexive_relation
+
+Like `ℕ`, {lean}`Prop` inhabits {lean}`Type`.
 -/
 example : Type := Prop
 /-
@@ -106,7 +109,7 @@ example : Type := Prop
 tag := "sec-definitional-equality-naive"
 %%%
 
-If two expressions are [definitionally equal][def-eq], then their equality can be proven using {lean}`rfl`. A sufficient (but not necessary) condition for definitional equality is that the expressions have the same normal form; the `#reduce` {index}[#reduce] command displays this normal form.{margin}[Further details of the reduction to normal form will be provided in due course. A {ref "sec-definitional-equality"}[summary] covering all aspects of definitional equality is also available.]
+If two expressions are [definitionally equal][def-eq], then their equality can be proven using {lean}`rfl`. A sufficient (but not necessary) condition for definitional equality is that the expressions have the same normal form. The `#reduce` {index}[#reduce] command displays this normal form.{margin}[Further details of the reduction to normal form will be provided in due course. A {ref "sec-definitional-equality"}[summary] covering all aspects of definitional equality is also available.]
 
 [def-eq]: https://lean-lang.org/doc/reference/latest/The-Type-System/#--tech-term-definitional-equality
 
@@ -118,7 +121,7 @@ Since the normal form of `1 - 1` is `0`, we can use {lean}`rfl` to prove `1 - 1 
 example : 1 - 1 = 0 := rfl
 /-
 
-By default, `#reduce` does not reduce inside types. This matters for equality, since an expression such as `a = a` is itself a type, namely `Eq a a`. We will {ref "sec-equality"}[return] to this later.
+By default, `#reduce` does not reduce inside types. This matters for equality, since it is itself a type.
 -/
 #reduce 1 - 1 = 0
 /-
@@ -137,9 +140,7 @@ example : 1 = 0 := rfl
 
 # Universe hierarchy
 
-The infinite sequence `Prop, Type 0, Type 1, …` is syntactic sugar for the universe hierarchy `Sort 0, Sort 1, Sort 2, …`. Here `Sort u` is called a universe and `u` is its level.
-
-We can verify that the two sequences coincide using {lean}`rfl`.
+The infinite sequence `Prop, Type 0, Type 1, …` is syntactic sugar for the universe hierarchy `Sort 0, Sort 1, Sort 2, …`. Here `Sort u` is called a universe and `u` is its level. We can verify that the two sequences coincide using {lean}`rfl`.
 -/
 example : Prop = Sort 0 := rfl
 example : Type u = Sort (u + 1) := rfl
@@ -150,7 +151,7 @@ The type of a universe is the next universe in the hierarchy.
 example : Sort (u + 1) := Sort u
 /-
 
-Each type `α` satisfies `α : Sort u` for exactly one `u = 0, 1, …`. We say that `Sort u` is the universe of `α` or that `α` inhabits `Sort u`.
+Each type `α` inhabits `Sort u` for exactly one `u = 0, 1, …`. We say that this `Sort u` is the universe of `α`.
 
 
 # Further proofs
