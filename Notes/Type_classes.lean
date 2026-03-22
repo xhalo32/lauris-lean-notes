454da9ac049360ffa472c0edbb8408b557735005
Lauri Oksanen <lauri.oksanen@helsinki.fi>
2026-03-22 07:17:25 +0200

    Added Type classes

---
+++ Notes/Type_classes.lean
@@ -0,0 +1,144 @@
+/-
+Type classes
+%%%
+tag := "sec-type-classes"
+%%%
+-/
+import Mathlib
+/-
+One of the simplest type classes is `Add`.
+-/
+#print Add
+/-
+
+The syntax of type class declarations is similar to structures. {index}[`class … where`] We define our version of `Add`.
+-/
+class Add' (α : Type u) where
+  add : α → α → α
+/-
+The `class` declaration results in formation an inductive type, just as if `structure` had been used instead.
+-/
+example (α : Type u) : Type u := Add' α
+/-
+
+Let us return to our version of natural numbers and define addition using structural recursion.
+-/
+inductive Nat' : Type where
+  | zero : Nat'
+  | succ : Nat' → Nat'
+
+def Nat'.add (x y : Nat') :=
+  match y with
+  | Nat'.zero => x
+  | Nat'.succ z => Nat'.succ (add x z)
+/-
+
+The syntax of instance declarations is similar to that of definitions using `def`. {index}[`instance … where`]
+-/
+instance instAdd'Nat' : Add' Nat' where
+  add := Nat'.add
+
+example : Add' Nat' := instAdd'Nat'
+/-
+
+Giving a name is optional in instance declarations, and Lean's instance synthesis can search for an expression of a required type. Instance synthesis can be tested using `#synth` command. {index}[`#synth`]
+-/
+#synth Add' Nat'
+/-
+
+Type classes enable [ad-hoc polymorphism][ad-hoc-polymorphism]. As an illustration, we define _add in reverse order_ on `Add'`. {index}[`[…]`]
+
+[ad-hoc-polymorphism]: https://en.wikipedia.org/wiki/Ad_hoc_polymorphism
+
+-/
+def Add'.addr {α : Type u} [Add' α] (x y : α) : α :=
+  Add'.add y x
+/-
+Here `[Add' α]` denotes an implicit argument of type `Add' α`. Now `Add'.addr` can be used on `Nat'`.
+-/
+example (x y : Nat') : Nat' := Add'.addr x y
+
+example (x y : Nat') : Add'.addr x y = Nat'.add y x := rfl
+/-
+In these two examples, Lean uses instance synthesis to supply an implicit argument of type `Add' Nat'`. The corresponding explicit versions read
+-/
+example (x y : Nat') : Nat' :=
+  @Add'.addr Nat' instAdd'Nat' x y
+
+example (x y : Nat')
+  : @Add'.addr Nat' instAdd'Nat' x y = Nat'.add y x
+:= rfl
+/-
+
+
+# Overloading
+
+The type class `HAdd` is responsible for the `+` notation. It allows heterogeneous addition, where the domain and codomain may differ.
+-/
+#print HAdd
+
+example (α β γ : Type) [HAdd α β γ]
+  (a : α) (b : β) : a + b = HAdd.hAdd a b := rfl
+/-
+
+We can [overload][overloading] `+` notation for `Add'`.
+
+[overloading]: https://en.wikipedia.org/wiki/Operator_overloading
+-/
+instance (α : Type u) [Add' α] : HAdd α α α where
+  hAdd := Add'.add
+/-
+
+Now `+` can be used on `Nat'`
+-/
+example (x y : Nat') : Nat' := x + y
+
+example (x y : Nat') : x + y = Nat'.add x y := rfl
+/-
+
+The standard version `Add` overloads `+` in the same way. This can be shown using `infer_instance` tactic.
+-/
+example (α : Type u) [Add α] : HAdd α α α
+:= by infer_instance
+/-
+
+
+# Class hierarchy
+
+The mathematical concept corresponding to `Add` is [magma][magma]. A [semigroup][semigroup] is a magma whose binary operation is associative. In Lean, there are parallel hierarchies for multiplication and addition. The two hierarchies are isomorphic from the mathematical point of view, only the notation differs.
+
+[magma]: https://en.wikipedia.org/wiki/Magma_(algebra)
+[semigroup]: https://en.wikipedia.org/wiki/Semigroup
+
+-/
+#print Semigroup
+#print AddSemigroup
+
+class AddSemigroup' (G : Type u) extends Add' G where
+  add_assoc : ∀ a b c : G, (a + b) + c = a + (b + c)
+/-
+
+All instances of `AddSemigroup'` are instances of `Add'`.
+-/
+example (G : Type u) [AddSemigroup' G] : Add' G
+:= by infer_instance
+/-
+
+Constructing an expression of type `AddSemigroup' Nat'` amounts to providing a proof that `Nat'.add` is associative.
+-/
+lemma Nat'.add_assoc (a b c : Nat')
+  : Nat'.add (Nat'.add a b) c = Nat'.add a (Nat'.add b c)
+:= by
+  induction c with
+  | zero => rfl
+  | succ c hi => simp [Nat'.add, hi]
+
+instance : AddSemigroup' Nat' where
+  add_assoc := Nat'.add_assoc
+/-
+We state associativity for the underlying operation `Nat'.add`, rather than the notation `+`. Using the notation `+` would require unfolding the abstraction provided by the type class `HAdd`.
+
+Mathlib has a rich hierarchy of classes. [Mathematics in Lean][mathematics-in-lean] gives an introduction to this hierarchy.
+
+[mathematics-in-lean]: https://leanprover-community.github.io/mathematics_in_lean
+-/
