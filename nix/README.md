3d8d67fd3708d9b6986698ef3183a5a0b466c09a
Niklas Halonen <niklas.halonen@protonmail.com>
2026-03-30 08:16:25 +0300

    Add nix infrastructure, bump deps to 4.29.0

---
+++ nix/README.md
@@ -0,0 +1,32 @@
+# Nix
+
+Nix is a package manager and a build system.
+It's an alternative to installing dependencies and building the document.
+The goal is to be hermetic and reproducible.
+
+As mathlib is currently not in the nix cache ([see this issue](https://github.com/NixOS/nixpkgs/issues/510957)) one has to build and cache it locally.
+
+## Developing locally
+
+[default.nix](./default.nix) provides required dependencies as well as convenience scripts `*-document`:
+
+```
+nix-shell nix -A shell
+preprocess-document
+generate-document
+postprocess-document
+python -m http.server -d generated/_out/html-multi
+```
+
+> Notice that entering the shell for the first time requires building mathlib.
+
+Alternatively one can use [direnv](https://github.com/nix-community/nix-direnv) to load the shell upon entering the repository.
+
+## Building the document
+
+`default.nix` provides a `notes` attribute which is the full generated HTML page.
+
+```
+nix-build nix -A notes
+python -m http.server -d result/
+```
---
+++ nix/default.nix
@@ -0,0 +1,123 @@
+{
+  sources ? import ./npins,
+  system ? builtins.currentSystem,
+  pkgs ? import sources.nixpkgs {
+    inherit system;
+    config = { };
+    overlays = [ (import sources.lean-blog { inherit pkgs; }).overlay ];
+  },
+}:
+let
+  lib = pkgs.lib;
+  inherit (import ./util.nix { inherit pkgs; }) mkOverridesFile;
+
+  pythonWithDeps = pkgs.python3.withPackages (
+    ps: with ps; [
+      beautifulsoup4
+      html5lib
+    ]
+  );
+in
+rec {
+  shell = pkgs.mkShell {
+    inputsFrom = [ notes ];
+    packages = with pkgs; [
+      pkgs.leanPackages.lean4
+      npins
+      jq
+      preprocess-document
+      postprocess-document
+      generate-document
+    ];
+    LAKE_PACKAGES = mkOverridesFile notes.passthru.allLeanDeps;
+  };
+
+  # We don't use init.fish because `lake init Document math` needs internet access plus it's unnecessary.
+  # Instead we provide our own lakefile.toml and lake-manifest.json.
+  # As the lake-manifest.json is empty, it only works if lake gets package-overrides through `--packages=...` as done in `generate-document` and buildLakePackage internally.
+  preprocess-document = pkgs.writeShellScriptBin "preprocess-document" ''
+    rm -rf generated
+    mkdir -p generated/Document
+    for file in Document/*.lean; do
+      echo "Preprocessing $file"
+
+      name=$(basename "''${file%.*}")
+      jq -Rsr -f scripts/preprocess.jq <$file >generated/Document/$(basename "$file")
+      echo "import Document.$name" >>generated/Document.lean
+    done
+
+    echo 'import Infra.Preamble' >>generated/Document.lean
+    cat Document.md >>generated/Document.lean
+
+    cp lean-toolchain generated/
+    cp -rfv verso/* generated/
+    cp ${./lakefile.toml} generated/lakefile.toml
+    # Copy empty manifest, lake must be used with --packages
+    cp ${./lake-manifest.json} generated/lake-manifest.json
+  '';
+
+  postprocess-document = pkgs.writeShellScriptBin "postprocess-document" ''
+    for file in $(find . -type f -name "index.html"); do
+      echo "Postprocessing $file"
+
+      ${lib.getExe pythonWithDeps} scripts/postprocess.py <$file >"$file"_pp
+      mv "$file"_pp $file
+    done
+  '';
+
+  # This is just for manual use
+  generate-document = pkgs.writeShellScriptBin "generate-document" ''
+    pushd generated
+    lake build --packages=${mkOverridesFile notes.passthru.allLeanDeps} Document
+    lake --no-ansi --packages=${mkOverridesFile notes.passthru.allLeanDeps} env lean --run Infra/Main.lean --output _out --verbose
+    cp -rfv ../assets/* _out/html-multi/
+    cp -rfv ../custom/* _out/html-multi/
+    popd
+  '';
+
+  notes =
+    let
+      # Recursively get lean deps
+      overridesFile = mkOverridesFile notes.passthru.allLeanDeps;
+    in
+    pkgs.leanPackages.buildLakePackage {
+      pname = "lean-notes";
+      version = "0.1.0";
+      src = pkgs.nix-gitignore.gitignoreSource [ ] ../.;
+
+      leanPackageName = "Document";
+
+      buildTargets = [ "Document" ];
+
+      preBuild = ''
+        ${lib.getExe preprocess-document}
+
+        # Move everything from generated back to root for building with buildLakePackage
+        cp -r generated/. .
+      '';
+
+      postBuild = ''
+        # Generate the HTML
+        lake --no-ansi --packages=${overridesFile} env lean --run Infra/Main.lean --output _out --verbose
+
+        cp -rfv ./assets/* _out/html-multi/
+        cp -rfv ./custom/* _out/html-multi/
+
+        ${lib.getExe postprocess-document}
+      '';
+
+      installPhase = ''
+        runHook preInstall
+        mkdir -p $out
+        cp -r _out/html-multi/. $out/
+        runHook postInstall
+      '';
+
+      leanDeps = [
+        pkgs.leanPackages.verso
+        pkgs.leanPackages.mathlib
+      ];
+    };
+
+  # TODO homework/exercises
+}
