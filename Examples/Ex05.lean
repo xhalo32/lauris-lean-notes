4aa6fc143c21096d729385e43499950b1f46fe12
Lauri Oksanen <lauri.oksanen@helsinki.fi>
2026-04-05 10:59:57 +0300

    Removed ugly rcases, added examples on obtain

--- Examples/Ex05.lean
+++ Examples/Ex05.lean
@@ -124,17 +124,12 @@ def swap {α β : Type} (s : α ⊕ β) : β ⊕ α :=
   | Sum.inl a => Sum.inr a
   | Sum.inr b => Sum.inl b
 
-example (α β : Type) (s : α ⊕ β)
-  : swap (swap s) = s
-:= by
-  cases s
-  · rfl
-  · rfl
-
 lemma swap_swap {α β : Type} (s : α ⊕ β)
   : swap (swap s) = s
 := by
-  cases s <;> rfl
+  cases s with
+  | inl a => rfl
+  | inr b => rfl
 
 example (α β γ : Type) : α ⊕ β ≃ β ⊕ α where
   toFun := swap
@@ -145,6 +140,35 @@ example (α β γ : Type) : α ⊕ β ≃ β ⊕ α where
   right_inv := by
     intro s
     exact swap_swap s
+/-
+
+Here are some variations of `swap_swap`.
+-/
+example (α β : Type) (s : α ⊕ β)
+  : swap (swap s) = s
+:= by
+  cases s with
+  | inl => rfl
+  | inr => rfl
+
+example (α β : Type) (s : α ⊕ β)
+  : swap (swap s) = s
+:= by
+  cases s
+  · rfl
+  · rfl
+
+example (α β : Type) (s : α ⊕ β)
+  : swap (swap s) = s
+:= by
+  cases s <;> rfl
+
+example (α β : Type) (s : α ⊕ β)
+  : swap (swap s) = s
+:= by
+  obtain (a | b) := s
+  · rfl
+  · rfl
 /-
 
 Show `(α ⊕ β) ⊕ γ ≃ α ⊕ (β ⊕ γ)`.
@@ -196,6 +220,16 @@ example (α β γ : Type) : (α ⊕ β) ⊕ γ ≃ α ⊕ (β ⊕ γ) where
     exact assoc_un s
 /-
 
+Here is a variation of `un_assoc`.
+-/
+example (α β γ : Type) (s : (α ⊕ β) ⊕ γ)
+  : unassoc (assoc s) = s
+:= by
+  obtain ((a | b) | c) := s
+  · rfl
+  · rfl
+  . rfl
+/-
 
 # Products and sums together
 
@@ -244,12 +278,143 @@ example (α β γ : Type) : α × (β ⊕ γ) ≃ (α × β) ⊕ (α × γ)
   right_inv := by
     intro s
     exact distrib_factor s
+
+-- Here is a variation of `factor_distrib`
+example (α β γ : Type) (p : α × (β ⊕ γ))
+  : factor (distrib p) = p
+:= by
+  obtain ⟨a, (b | c)⟩ := p
+  · rfl
+  · rfl
+
+-- Here is a variation of `distrib_factor`
+example (α β γ : Type) (s : (α × β) ⊕ (α × γ))
+  : distrib (factor s) = s
+:= by
+  obtain (⟨a, b⟩ | ⟨a, c⟩) := s
+  · rfl
+  · rfl
 /-
 
 
-# A uniqueness principle for pairing
+# Function from a sum
 
-If two functions into a product have equal first and second projections, then the functions are equal.
+Function from a disjoint union is determined by its restrictions.
+-/
+example (α β γ : Type) (f g : α ⊕ β → γ) (x : α ⊕ β)
+  (hl : ∀ a, f (Sum.inl a) = g (Sum.inl a))
+  (hr : ∀ b, f (Sum.inr b) = g (Sum.inr b))
+  : f x = g x
+:= by
+  obtain (a | b) := x
+  · -- 1st case x = a : α
+    -- __Solution__
+    exact hl a
+  · -- 2nd case x = b : β
+    -- __Solution__
+    exact hr b
+
+-- __Solution__ using grind
+example (α β γ : Type) (f g : α ⊕ β → γ) (x : α ⊕ β)
+  (hl : ∀ a, f (Sum.inl a) = g (Sum.inl a))
+  (hr : ∀ b, f (Sum.inr b) = g (Sum.inr b))
+  : f x = g x
+:= by
+  grind
+/-
+
+A slightly harder variant.
+-/
+example (α β γ : Type) (f g : α ⊕ β → γ)
+  (hl : (λ a ↦ f (Sum.inl a)) = (λ a ↦ g (Sum.inl a)))
+  (hr : (λ b ↦ f (Sum.inr b)) = (λ b ↦ g (Sum.inr b)))
+  : f = g
+:= by
+  -- __Solution__
+  funext x
+  obtain (a | b) := x
+  · calc
+    f (Sum.inl a)
+    _ = (λ a ↦ f (Sum.inl a)) a := by rfl
+    _ = (λ a ↦ g (Sum.inl a)) a := by rw [hl]
+    _ = g (Sum.inl a) := by rfl
+  · calc
+    f (Sum.inr b)
+    _ = (λ b ↦ f (Sum.inr b)) b := by rfl
+    _ = (λ b ↦ g (Sum.inr b)) b := by rw [hr]
+    _ = g (Sum.inr b) := by rfl
+/-
+
+
+# Pairs and equality
+
+Extensionality of pairs.
+-/
+lemma prod_ext {α β : Type} {p q : α × β}
+  (h1 : p.1 = q.1)
+  (h2 : p.2 = q.2)
+  : p = q
+:= by
+  obtain ⟨p1, p2⟩ := p
+  obtain ⟨q1, q2⟩ := q
+  cases h1
+  cases h2
+  rfl
+
+example (α β : Type) (p q : α × β)
+  (h1 : p.1 = q.1)
+  (h2 : p.2 = q.2)
+  : p = q
+:= by
+  grind
+/-
+
+Equality of pairs implies equality of components.
+-/
+example (α β : Type) (p q : α × β)
+  (h : p = q)
+  : p.1 = q.1
+:= by
+  -- __Solution__
+  cases h
+  rfl
+
+-- __Solution__ by grind
+example (α β : Type) (p q : α × β)
+  (h : p = q)
+  : p.1 = q.1
+:= by
+  grind
+/-
+
+
+# Function to a product
+
+Function to a product is determined by its components.
+-/
+example (α β γ : Type) (f g : α → β × γ) (a : α)
+  (h1 : ∀ a, (f a).1 = (g a).1)
+  (h2 : ∀ a, (f a).2 = (g a).2)
+  : f a = g a
+:= by
+  apply prod_ext
+  · -- 1st case/component
+    -- __Solution__
+    exact h1 a
+  · -- 2nd case/component
+    -- __Solution__
+    exact h2 a
+
+-- __Solution__ using grind
+example (α β γ : Type) (f g : α → β × γ) (a : α)
+  (h1 : ∀ a, (f a).1 = (g a).1)
+  (h2 : ∀ a, (f a).2 = (g a).2)
+  : f a = g a
+:= by
+  grind
+/-
+
+A slightly harder variant.
 -/
 example (α β γ : Type) (f g : α → β × γ)
   (h1 : (λ a ↦ (f a).1) = (λ a ↦ (g a).1))
@@ -257,21 +422,18 @@ example (α β γ : Type) (f g : α → β × γ)
   : f = g
 := by
   funext a
-  rcases hf : f a with ⟨f1, f2⟩
-  rcases hg : g a with ⟨g1, g2⟩
-  have hl := calc
-    f1
-    _ = (f a).1 := by rw [hf]
+  apply prod_ext
+  · -- 1st case/component
+    -- __Solution__
+    calc
+    (f a).1
     _ = (λ a ↦ (f a).1) a := by rfl
     _ = (λ a ↦ (g a).1) a := by rw [h1]
     _ = (g a).1 := by rfl
-    _ = g1 := by rw [hg]
-  -- __Solution__
-  have hr := calc
-    f2
-    _ = (f a).2 := by rw [hf]
+  · -- 2nd case/component
+    -- __Solution__
+    calc
+    (f a).2
     _ = (λ a ↦ (f a).2) a := by rfl
     _ = (λ a ↦ (g a).2) a := by rw [h2]
     _ = (g a).2 := by rfl
-    _ = g2 := by rw [hg]
-  rw [hl, hr]
