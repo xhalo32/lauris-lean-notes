c26bb7816d0783a8079efb1fe4d83013db838982
Lauri Oksanen <lauri.oksanen@helsinki.fi>
2026-04-05 11:00:28 +0300

    Added reminder to cover equivalence

--- Examples/Ex06.lean
+++ Examples/Ex06.lean
@@ -1,4 +1,29 @@
+import Mathlib
 /-
+
+# Equivalence
+
+Equivalence `p ↔ q` is a product type like `And`. It is implemented as a structure. Find this structure and write your own version of it.
+-/
+-- __Solution__
+variable (p q : Prop)
+set_option pp.notation false in
+#reduce p ↔ q
+
+example (p q : Prop) : (p ↔ q) = (Iff p q) := rfl
+
+#print Iff
+
+structure Iff' (p q : Prop) : Prop where
+  mp : p → q
+  mpr : q → p
+/-
+
+Formulate and prove [biconditional elimination](https://en.wikipedia.org/wiki/Biconditional_elimination).
+-/
+
+/-
+
 
 Should contain at least the following analogues of
 
