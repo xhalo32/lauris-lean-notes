df1ebf880ee6b60b60cf2e9747b78d736b780a85
Lauri Oksanen <lauri.oksanen@helsinki.fi>
2026-04-12 11:04:16 +0300

    Added elementary proofs by calculation

--- Examples/Ex01.lean
+++ Examples/Ex01.lean
@@ -1,33 +1,65 @@
 import Mathlib
+/-
 
-example (x y : ℝ)
-  : x^2 - y^2 = (x + y) * (x - y)
-:= by grind
+# Proofs using automation
 
+-/
 lemma diff_sq (x y : ℝ)
   : x^2 - y^2 = (x + y) * (x - y)
-:= by grind
+:= by
+  grind
 
 #print diff_sq
 #print diff_sq._proof_1_1
 /-
-Proof uses
+
+The proof uses
 
 * properties of ℝ as a commutative ring
-* classical logic (grind uses techniques inspired by modern [SMT][SMT] solvers, it always attempts to derive a contradiction)
+* classical logic
 
-Lean consists of
-
-* High level automation
-* Low level proof checked by a relatively simple (< 9000 lines of code) program called the kernel.
+The `grind` tactic is similar to modern [SMT][SMT] solvers. It generates proofs by contradiction.
 
 [SMT]: https://en.wikipedia.org/wiki/Satisfiability_modulo_theories
 
+Show [Brahmagupta's identity][brahmagupta].
+
+[brahmagupta]: https://en.wikipedia.org/wiki/Brahmagupta%27s_identity
+
+-/
+example (n a b c d : ℝ)
+  : (a^2 + n*b^2) * (c^2 + n*d^2)
+    = (a*c - n*b*d)^2 + n*(a*d + b*c)^2
+:= by
+  -- __Solution__
+  grind
+/-
+
+Show [Woodbury formula][woodbury] for scalars.
+
+[woodbury]: https://en.wikipedia.org/wiki/Woodbury_matrix_identity
+
+-/
+example (a c u v : ℝ)
+  (h1 : a + u*c*v ≠ 0) (h2 : a ≠ 0) (h3 : c ≠ 0)
+  : (a + u*c*v)⁻¹ = a⁻¹ - a⁻¹*u * (c⁻¹ + v*a⁻¹*u)⁻¹ * v*a⁻¹
+:= by
+  -- __Solution__
+  grind
+/-
+
+
+# Step by step proofs
+
+Consider the following commutation.
 -/
 example (a b c : ℕ)
   : a * b * c = b * c * a
 := by grind
+/-
 
+In an abstract form, the commutation reads
+-/
 example (G : Type) [Semigroup G] (a b c : G)
   (h1 : a * b = b * a)
   (h2 : a * c = c * a)
@@ -60,14 +92,62 @@ example (G : Type) [Semigroup' G] (a b c : G)
   _ = b * (a * c) := by rw [Semigroup'.mul_assoc]
   _ = b * (c * a) := by rw [h2]
   _ = b * c * a := by rw [Semigroup'.mul_assoc]
+/-
 
+We can replace the hypotheses `h1` and `h2` by the stronger hypothesis that the semigroup is commutative. Let us start with a single step.
+-/
 example (G : Type) [Semigroup' G] (a b c : G)
-  (h1 : ∀ x y : G, x * y = y * x)
+  (h : ∀ x y : G, x * y = y * x)
+  : a * b * c = b * a * c
+:= by
+  rw [h a b]
+/-
+
+Show
+-/
+example (G : Type) [Semigroup' G] (a b c : G)
+  (h : ∀ x y : G, x * y = y * x)
   : a * b * c = b * c * a
 := by
+  -- __Solution__
   calc
     a * b * c
-    _ = b * a * c := by rw [h1 a b]
+    _ = b * a * c := by rw [h a b]
     _ = b * (a * c) := by rw [Semigroup'.mul_assoc]
-    _ = b * (c * a) := by rw [h1 a c]
+    _ = b * (c * a) := by rw [h a c]
     _ = b * c * a := by rw [Semigroup'.mul_assoc]
+/-
+
+
+# Proofs with steps and automation
+
+Show an elementary case of [Young's inequality][young].
+
+[young]: https://en.wikipedia.org/wiki/Young%27s_inequality_for_products#Elementary_case
+
+-/
+lemma elem_young (a b : ℝ) :
+  a * b ≤ a^2/2 + b^2/2
+:= by
+  have : (a - b)^2 ≥ 0 := by positivity
+  grind
+/-
+
+
+Show [Peter-Paul][peter-paul] inequality.
+
+[peter-paul]: https://en.wikipedia.org/wiki/Young%27s_inequality_for_products#Elementary_case
+
+-/
+example (a b ε : ℝ) (h : ε > 0) :
+  a * b ≤ a^2/(2*ε) + ε*b^2/2
+:= calc
+  a * b
+  _ = (a*(√ε)⁻¹) * (√ε*b) := by
+    -- __Solution__
+    grind
+  _ ≤ (a*(√ε)⁻¹)^2/2 + (√ε*b)^2/2 := by
+    exact elem_young (a*(√ε)⁻¹) (√ε*b)
+  _ = a^2/(2*ε) + ε*b^2/2 := by
+    -- __Solution__
+    grind
